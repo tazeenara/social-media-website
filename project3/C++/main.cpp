a3fe6addf538d2749af3029612b4067875f202b6
Tazeen Ara <141852741+tazeenara@users.noreply.github.com>
2023-08-10 15:57:30 +0530

    Add files via upload

---
+++ project3/C++/main.cpp
@@ -0,0 +1,88 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    // cout<<"Hello World"<<endl;
+    // cout<<"hello everyone";
+
+    // int a, b, c;
+    // short sa =28;
+    // cout<<sa;
+
+    // camel case notation
+    // int marksInMaths=88;
+    // cout<<"The marks of The Student in Maths is "<<marksInMaths;
+
+// string
+// short a;
+// int b=77;
+// long c;
+// long long d; 
+// float  const score=34;
+// double score2=56;
+// long double score3=78;
+// // score=88.9;
+// b=44;
+// cout<<"The score is "<<score;
+
+// int a, b;
+// cout<<"Enter your First Number"<<endl;
+// cin>>a;
+
+// cout<<"Enter your Second Number"<<endl;
+// cin>>b;
+// // cout<<" The Sum is "<<a+b;
+
+// cout<<" a + b is "<<a + b<<endl;
+// cout<<" a - b is "<<a - b<<endl;
+// cout<<" a * b is "<<a * b<<endl;
+// cout<<" a / b is "<<(float) a / b<<endl;
+
+// int age;
+// cout<<"Enter Your age"<<endl;
+// cin>>age;
+
+// switch (age)
+// {
+// case 12:
+//     cout<<"You are 12 years old";
+//     break;
+//     case 18:
+//     cout<<"You are 18 years old";
+//     break;
+
+// default:
+// cout<<"You are neither 12 or 18 years old";
+//     break;
+// }
+
+// int index =0;
+// while(index<34)
+// {
+//     cout<<"we are at index number"<<index<<endl;
+//     index= index +1;
+// }
+
+
+do
+{
+cout<<"we are at index number "<<index<<endl;
+index= index+ 1;
+} while (index>456)
+
+
+
+
+// if (age>150){
+//     cout<<"Invalid";
+// }
+//  else if(age>=18){
+//     cout<<"You can Vote";
+// }
+// else{
+//     cout<<"You can not vote";
+// }
+
+
+    return 0;
+}
